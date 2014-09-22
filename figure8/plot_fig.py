ebf15e9fb07f6a8a5ac53ed5690e1560cec4e050
Vincent Gauthier <pub@luxbulb.org>
2014-09-22 16:17:24 +0200

    update the figure 8

--- figure8/plot_fig.py
+++ figure8/plot_fig.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 #matplotlib config
-def matplotlib_setup(figsize_x=8.3, figsize_y=4.2):
+def matplotlib_setup(figsize_x=15, figsize_y=5):
     import matplotlib as mpl
     cmap = cm.jet
     mpl.rcParams['axes.labelsize'] = 9
@@ -13,7 +13,6 @@ def matplotlib_setup(figsize_x=8.3, figsize_y=4.2):
     #rcParams['font.family'] = 'serif'
     #rcParams['font.serif'] = ['Computer Modern Roman']
     mpl.rcParams['text.usetex'] = True
-    mpl.rcParams['figure.figsize'] = 7.3, 4.2
     mpl.rcParams['axes.labelweight'] = 'bold'
     mpl.rcParams['font.weight'] = 'bold'
     mpl.rcParams['axes.linewidth'] = 0.75
@@ -26,21 +25,21 @@ def matplotlib_setup(figsize_x=8.3, figsize_y=4.2):
     mpl.rcParams['figure.figsize'] = figsize_x, figsize_y
     # figure dots per inch
     mpl.rcParams['figure.dpi'] = 300
-    mpl.rcParams['figure.autolayout'] = True
+    #mpl.rcParams['figure.autolayout'] = True
 
 #Load curve data subplot a
 perco = []; rand = []; corre = []; std_perco = []; std_rand = []; std_corre = []
-with open('fig_percentage\percentage_percolation.json', "r") as fichier:
+with open('fig_percentage/percentage_percolation.json', "r") as fichier:
     perco = json.load(fichier)
-with open('fig_percentage\std_percolation.json', "r") as fichier:
+with open('fig_percentage/std_percolation.json', "r") as fichier:
     std_perco = json.load(fichier)
-with open('fig_percentage\percentage_random.json', "r") as fichier:
+with open('fig_percentage/percentage_random.json', "r") as fichier:
     rand = json.load(fichier)
-with open('fig_percentage\std_random.json', "r") as fichier:
+with open('fig_percentage/std_random.json', "r") as fichier:
     std_rand = json.load(fichier)
-with open('fig_percentage\percentage_correlated.json', "r") as fichier:
+with open('fig_percentage/percentage_correlated.json', "r") as fichier:
     corre = json.load(fichier)
-with open('fig_percentage\std_correlated.json', "r") as fichier:
+with open('fig_percentage/std_correlated.json', "r") as fichier:
     std_corre = json.load(fichier)
 
 #Transform data
@@ -53,17 +52,17 @@ corre_inf = [a-b for a,b in zip(corre,std_corre)]
 
 #Load curve data subplot b
 perco2 = []; rand2 = []; corre2 = []; std_perco2 = []; std_rand2 = []; std_corre2 = []
-with open('fig_SW\percentage_percolation.json', "r") as fichier:
+with open('fig_SW/percentage_percolation.json', "r") as fichier:
     perco2 = json.load(fichier)
-with open('fig_SW\std_percolation.json', "r") as fichier:
+with open('fig_SW/std_percolation.json', "r") as fichier:
     std_perco2 = json.load(fichier)
-with open('fig_SW\percentage_random.json', "r") as fichier:
+with open('fig_SW/percentage_random.json', "r") as fichier:
     rand2 = json.load(fichier)
-with open('fig_SW\std_random.json', "r") as fichier:
+with open('fig_SW/std_random.json', "r") as fichier:
     std_rand2 = json.load(fichier)
-with open('fig_SW\percentage_correlated.json', "r") as fichier:
+with open('fig_SW/percentage_correlated.json', "r") as fichier:
     corre2 = json.load(fichier)
-with open('fig_SW\std_correlated.json', "r") as fichier:
+with open('fig_SW/std_correlated.json', "r") as fichier:
     std_corre2 = json.load(fichier)
 
 #Transform data
@@ -75,11 +74,11 @@ corre_sup2 = [a+b for a,b in zip(corre2,std_corre2)]
 corre_inf2 = [a-b for a,b in zip(corre2,std_corre2)]
 
 #make the plot
-matplotlib_setup(4,3)
+matplotlib_setup()
 Pmax = 11; phi = 0.15; alph = .3
 
 fig = plt.figure()
-ax = fig.add_subplot(121)
+ax = fig.add_subplot(131)
 ax.set_ylim([0,3])
 ax.plot( range(Pmax)[1:], perco2, 'o--', color='b', label='percolation')
 ax.plot( range(Pmax)[1:], rand2, 'o--', color='r', label='random')
@@ -97,7 +96,7 @@ ax.grid()
 
 Pmax = 12; phi = 0.15; nb_coal = 7; alph = .3
 
-ax2 = fig.add_subplot(122)
+ax2 = fig.add_subplot(132)
 ax2.set_ylim([0,1.2])
 ax2.set_xlim([0,11])
 ax2.plot( range(Pmax), perco, 'o--', color='b', label='percolation')
@@ -109,11 +108,11 @@ ax2.fill_between(range(Pmax), rand_sup, rand, facecolor='red', alpha=alph)
 ax2.fill_between(range(Pmax), rand, rand_inf, facecolor='red', alpha=alph)
 ax2.fill_between(range(Pmax), corre_sup, corre, facecolor='green', alpha=alph)
 ax2.fill_between(range(Pmax), corre, corre_inf, facecolor='green', alpha=alph)
-ax2.text(1.5, 0.95, 'b', transform=ax.transAxes, fontsize=16, fontweight='bold', va='top')
-ax2.legend(bbox_to_anchor=(1.05, 1), loc=2, ncol=3, fancybox=True, shadow=False)
+ax2.text(1.25, 0.95, 'b', transform=ax.transAxes, fontsize=16, fontweight='bold', va='top')
+lgd = ax2.legend(bbox_to_anchor=(1.1, 1.02), loc=2)
 ax2.set_xlabel(r" \textbf{ $ P_{\phi}^{MIN}$ }",fontsize=14)
 ax2.set_ylabel('Valid coalition percentage')
 ax2.grid()
-plt.savefig('fig8.pdf')
+plt.savefig('fig8.pdf', pad_inches=6)
 plt.savefig('fig8.eps')
-plt.savefig('fig8.png')
+#plt.savefig('fig8.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
